e1c1e43413d9e467ebf40e50bf31c7b80ba2c78d
Josh Rickard <josh.rickard@swimlane.com>
2020-02-19 09:59:28 -0600

    modified test.py

--- bin/test.py
+++ bin/test.py
@@ -2,5 +2,14 @@ from pyattck import Attck
 
 attack = Attck()
 
-for technique in attack.techniques:
-    print(technique.name)
+for actor in attack.enterprise.actors:
+    print(actor.name)
+    print(actor.ascii_logo)
+    print(actor.image_logo)
+
+#for tool in attack.enterprise.tools:
+#    print(tool.name)
+#    print(tool.c2_data)
+
+
+print(attack.enterprise.search_commands('powershell'))
