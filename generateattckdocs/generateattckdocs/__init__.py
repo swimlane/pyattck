324755a3e6946a1ba222b5eb827eab459046f313
Josh Rickard <josh.rickard@Joshs-MacBook-Pro.local>
2020-04-28 15:58:30 -0500

    adding ability to generate attack docs

---
+++ generateattckdocs/generateattckdocs/__init__.py
@@ -0,0 +1,1 @@
+from generateattackdocs.generatedocs import GenerateDocs
---
+++ generateattckdocs/generateattckdocs/mitigationdocs.py
@@ -0,0 +1,53 @@
+import os
+
+
+from generateattackdocs.attckdocs import AttckDocs
+
+
+class MitigationDocs(AttckDocs):
+
+    def __init__(self, folder):
+        self.folder = os.path.abspath(folder)
+        if not os.path.exists(self.folder):
+            os.mkdir(self.folder)
+        if not os.path.exists(os.path.join(self.folder, 'mitigations')):
+            os.mkdir(os.path.join(self.folder, 'mitigations'))
+        
+        self.folder = os.path.join(self.folder, 'mitigations')
+        
+
+    def go(self):
+        for mitigation in self._attck.enterprise.mitigations:
+            markdown = None
+            markdown = '''
+# {name}
+
+## Description
+
+### MITRE Description
+
+> {description}
+
+'''.format(
+    name=mitigation.name,
+    description=mitigation.description,
+)
+            technique_list = None
+            for technique in mitigation.techniques:
+                if technique_list is None:
+                    technique_list = '''
+* [{name}](../techniques/{id}.md)
+'''.format(name=technique.name, id=technique.name.replace(' ', '-').replace('/','-'))
+                else:
+                    technique_list += '''
+* [{name}](../techniques/{id}.md)
+    '''.format(name=technique.name, id=technique.name.replace(' ', '-').replace('/','-'))
+            
+            markdown += '''
+# Techniques
+
+{technique_list}
+'''.format(technique_list=technique_list)
+
+            with open('{folder}/{name}.md'.format(folder=self.folder, name=mitigation.name.replace(' ', '-').replace('/','-')), 'w+') as f:
+                f.write(markdown)
