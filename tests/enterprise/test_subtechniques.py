9e5e56fb85a1ef91fc7748402e352232a2bb2747
Josh Rickard <josh.rickard@Joshs-MacBook-Pro.local>
2020-04-29 12:20:30 -0500

    modified tests fof subtechniques

--- tests/enterprise/test_subtechniques.py
+++ tests/enterprise/test_subtechniques.py
@@ -7,7 +7,8 @@ def test_subtechniques_have_tactics(attck_fixture_subtechniques):
     Args:
         attck_fixture ([type]): our default Mitre ATT&CK JSON fixture
     """
-    for technique in attck_fixture_subtechniques.enterprise.techniques:
-        if technique.subtechniques:
-            for subtechnique in technique.subtechniques:
-                assert getattr(subtechnique,'tactics')
+    if attck_fixture_subtechniques.subtechniques:
+        for technique in attck_fixture_subtechniques.enterprise.techniques:
+            if technique.subtechniques:
+                for subtechnique in technique.subtechniques:
+                    assert getattr(subtechnique,'tactics')
