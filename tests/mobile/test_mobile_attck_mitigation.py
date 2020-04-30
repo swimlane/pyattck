98400d4156c6ee392130aa4322698ad8e4b0d347
Josh Rickard <josh.rickard@Joshs-MacBook-Pro.local>
2020-04-29 22:18:42 -0500

    Adding tests for Mobile feature

---
+++ tests/mobile/test_mobile_attck_mitigation.py
@@ -0,0 +1,9 @@
+def test_mobile_attck_mitigation_have_techniques(attck_fixture):
+    """Some MITRE Mobile ATT&CK Mitigation should have Techniques
+    
+    Args:
+        attck_fixture ([type]): our default MITRE Mobile ATT&CK JSON fixture
+    """
+    for mitigation in attck_fixture.mobile.mitigations:
+        if mitigation.techniques:
+            assert getattr(mitigation,'techniques')
---
+++ tests/mobile/test_mobile_attck_tactics.py
@@ -0,0 +1,9 @@
+
+def test_mobile_attck_tactics_have_techniques(attck_fixture):
+    """All MITRE Mobile ATT&CK Techniques should have tactics
+    
+    Args:
+        attck_fixture ([type]): our default MITRE Mobile ATT&CK JSON fixture
+    """
+    for tactic in attck_fixture.mobile.tactics:
+        assert getattr(tactic,'techniques')
