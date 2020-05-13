38ceb277401fdf72fd709b55add63294c812cb59
Josh Rickard <josh.rickard@Joshs-MacBook-Pro.local>
2020-05-13 15:46:47 -0500

    Adding newbeeattackdataset data to generatedattacks

---
+++ generateattcks/generateattcks/newbeeattackdata.py
@@ -0,0 +1,28 @@
+import requests
+
+from .attacktemplate import AttackTemplate
+
+
+class NewBeeAttackDataset(object):
+
+    URL = 'https://raw.githubusercontent.com/NewBee119/Attack-Technique-Dataset/master/tech_refer.json'
+
+    def __get_data(self):
+        return requests.get(self.URL).json()
+
+    def get(self):
+        return_dict = {}
+        for key,val in self.__get_data().items():
+            for item in val:
+                if item not in return_dict:
+                    return_dict[item] = []
+                return_dict[item].append(key)
+
+        return_list = []
+        template = AttackTemplate()
+        for key,val in return_dict.items():
+            template.id = key
+            for item in val:
+                template.add_external_reference(item)
+            return_list.append(template.get())
+        return return_list
