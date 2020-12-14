b73db9dab993e8c330ce3eaf3abf8f45a5a24e34
Josh <josh.rickard@swimlane.com>
2020-12-13 23:05:50 -0600

    bumping version to 2.1.3

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ def parse_requirements(requirement_file):
 
 setup(
     name='pyattck',
-    version='2.1.2',
+    version='2.1.3',
     packages=find_packages(exclude=['tests*']),
     license='MIT',
     description='A Python package to interact with the Mitre ATT&CK Frameworks',
