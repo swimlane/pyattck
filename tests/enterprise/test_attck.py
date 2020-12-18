f2dca73ae46dd38f9dff4f801d02f23fb455bd0f
Josh <josh.rickard@swimlane.com>
2020-12-18 10:06:23 -0600

    Updated attck tests

--- tests/enterprise/test_attck.py
+++ tests/enterprise/test_attck.py
@@ -10,25 +10,11 @@ from os.path import expanduser
 )
 def test_attck_attribute_is_list(target_attribute):
     from pyattck import Attck
-    path = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'fixtures', 'generated_attck_data' + '.json')
-    attck = Attck(dataset_json=path)
+    path = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'fixtures')#, 'generated_attck_data' + '.json')
+    attck = Attck(data_path=path)
     enterprise = getattr(attck, 'enterprise')
     assert isinstance(
         getattr(enterprise, target_attribute),
-        list
-    )
-
-@pytest.mark.parametrize(
-    'target_attribute', 
-    ['techniques', 'tactics', 'mitigations', 'malwares', 'actors', 'tools']
-)
-def test_attck_attribute_is_list_deprecated(target_attribute):
-    from pyattck import Attck
-    path = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'fixtures', 'generated_attck_data' + '.json')
-    attck = Attck(dataset_json=path)
-
-    assert isinstance(
-        getattr(attck, target_attribute),
         list
     )
 
@@ -43,32 +29,11 @@ def test_attck_attribute_is_list_deprecated(target_attribute):
 
 def test_all_attck_objects_have_standard_properties(target_attribute,target_properties):
     from pyattck import Attck
-    path = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'fixtures', 'generated_attck_data' + '.json')
-    attck = Attck(dataset_json=path)
+    path = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'fixtures')#, 'generated_attck_data' + '.json')
+    attck = Attck(data_path=path)
     enterprise = getattr(attck, 'enterprise')
     return_list = []
     for attribute in getattr(enterprise,target_attribute):
-        if hasattr(attribute, target_properties):
-            return_list.append(getattr(attribute,target_properties))
-    if len(return_list) >= 1:
-        assert True
-
-
-@pytest.mark.parametrize(
-    'target_attribute', 
-    ['techniques', 'tactics', 'mitigations', 'malwares', 'actors', 'tools']
-)
-@pytest.mark.parametrize(
-    'target_properties',
-    ['id','name','description','reference','created','modified','stix','type']
-)
-
-def test_all_attck_objects_have_standard_properties_deprecated(target_attribute,target_properties):
-    from pyattck import Attck
-    path = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'fixtures', 'generated_attck_data' + '.json')
-    attck = Attck(dataset_json=path)
-    return_list = []
-    for attribute in getattr(attck,target_attribute):
         if hasattr(attribute, target_properties):
             return_list.append(getattr(attribute,target_properties))
     if len(return_list) >= 1:
