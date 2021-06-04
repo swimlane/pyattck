0a8a278ce958eb3bacd7fa0f5b4159b30295131d
Josh <josh.rickard@swimlane.com>
2021-06-04 11:29:11 -0500

    Removing support for Python2

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setup(
     url='https://github.com/swimlane/pyattck',
     author='Swimlane',
     author_email='info@swimlane.com',
-    python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*,!=3.3.*, !=3.4.*, !=3.5, <4',
+    python_requires='>=3.6, <4',
     package_data={
         'pyattck':  ['data/actors/*.png']
     },
