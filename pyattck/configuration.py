74cec05599305150f51313a327b120fc761a045f
Josh <josh.rickard@swimlane.com>
2021-07-29 09:32:12 -0500

    Updated configuration class

--- pyattck/configuration.py
+++ pyattck/configuration.py
@@ -1,58 +1,26 @@
 import os
 import json
 import warnings
-from pathlib import Path
 from urllib.parse import urlparse
-import attr
+from pathlib import Path
 import yaml
 from requests.api import request
 from .utils.exceptions import UknownFileError
 
 
-@attr.s(frozen=True)
-class Configuration:
+class ConfigurationProperties(type):
 
-    use_config            = attr.ib(default=False, type=bool)
-    save_config           = attr.ib(default=False, type=bool)
-    config_file_path      = attr.ib(default='~/pyattck/config.yml', type=str)
-    data_path             = attr.ib(default='~/pyattck/data', type=str)
-    enterprise_attck_json = attr.ib(default="https://raw.githubusercontent.com/mitre/cti/master/enterprise-attack/enterprise-attack.json", type=str)
-    pre_attck_json        = attr.ib(default="https://raw.githubusercontent.com/mitre/cti/master/pre-attack/pre-attack.json", type=str)
-    mobile_attck_json     = attr.ib(default="https://raw.githubusercontent.com/mitre/cti/master/mobile-attack/mobile-attack.json", type=str)
-    nist_controls_json    = attr.ib(default="https://raw.githubusercontent.com/center-for-threat-informed-defense/attack-control-framework-mappings/master/frameworks/nist800-53-r4/stix/nist800-53-r4-controls.json", type=str)
-    generated_attck_json  = attr.ib(default="https://github.com/swimlane/pyattck/blob/master/generated_attck_data.json?raw=True", type=str)
-    generated_nist_json   = attr.ib(default="https://github.com/swimlane/pyattck/blob/master/attck_to_nist_controls.json?raw=True", type=str)
-    requests_kwargs       = attr.ib(default={})
+    __config_data = None
 
-    @config_file_path.validator
-    def validate_config_file_path(cls, attribute, value):
-        print(value)
-        if value.endswith('.json') or value.endswith('.yml') or value.endswith('.yaml'):# or not cls.__check_if_path(value):
-            pass
-        else:
-            raise ValueError('Please provide a config_file_path with .json, .yml, or .yaml extension.')
+    def __download_url_data(cls, url):
+        return request('GET', url, **cls.requests_kwargs).json()
 
-    @data_path.validator
-    def validate_data_path(cls, attribute, value):
-        if not cls.__check_if_path(value):
-            raise ValueError('Please provide a directory for data_path value.')
-
-    @enterprise_attck_json.validator
-    @pre_attck_json.validator
-    @mobile_attck_json.validator
-    @nist_controls_json.validator
-    @generated_attck_json.validator
-    @generated_nist_json.validator
-    def validate_path_or_url(cls, attribute, value):
-        if not cls.__check_if_url(value) or not cls.__check_if_path(value):
-            raise ValueError('Please provide a URl or path string as a value for any json files.')
-
-    def __check_if_path(cls, value):
+    def _check_if_path(cls, value):
         if Path(value):
             return True
         return False
 
-    def __check_if_url(cls, value):
+    def _check_if_url(cls, value):
         try:
             if urlparse(value).scheme in ['http', 'https']:
                 return True
@@ -60,14 +28,22 @@ class Configuration:
         except:
             return False
 
-    def __attrs_post_init__(self):
-        if self.save_config:
-            print(self)
-            print(attr.asdict(self))
-            self.__save_config(self.config_file_path, self)
+    def __get_absolute_path(cls, path_string):
+        return os.path.abspath(
+            os.path.expanduser(
+                os.path.expandvars(path_string)
+            )
+        )
 
-    def __save_config(cls, path, data):
-        # save configuration to disk
+    def __validate_value_string(cls, value):
+        if cls._check_if_url(value):
+            return value
+        elif cls._check_if_path(value):
+            return value
+        else:
+            raise Exception('The provided value is neither a URL or file path')
+
+    def __write_to_disk(cls, path, data):
         if not os.path.exists(os.path.dirname(path)):
             try:
                 os.makedirs(os.path.dirname(path))
@@ -81,6 +57,19 @@ class Configuration:
             else:
                 raise UknownFileError(provided_value=path, known_values=['.json', '.yml', '.yaml'])
 
+    def __read_from_disk(cls, path):
+        if os.path.exists(path) and os.path.isfile(path):
+            try:
+                with open(path) as f:
+                    if path.endswith('.json'):
+                        return json.load(f)
+                    elif path.endswith('.yml') or path.endswith('.yaml'):
+                        return yaml.load(f, Loader=yaml.FullLoader)
+                    else:
+                        raise UknownFileError(provided_value=path, known_values=['.json', '.yml', '.yaml'])
+            except:
+                pass
+        return None
 
     def _save_json_data(cls, force: bool=False) -> None:
         if not os.path.exists(cls.data_path):
@@ -100,3 +89,145 @@ class Configuration:
                     cls.__write_to_disk(path, data)
                 setattr(cls, '_' + json_data, path)
         cls.__update_config()
+
+    def __update_config(cls):
+        cls.__config_data = {
+            'data_path': cls.data_path,
+            'enterprise_attck_json': cls._enterprise_attck_json,
+            'pre_attck_json': cls._pre_attck_json,
+            'mobile_attck_json': cls._mobile_attck_json,
+            'nist_controls_json': cls._nist_controls_json,
+            'generated_attck_json': cls._generated_attck_json,
+            'generated_nist_json': cls._generated_nist_json,
+            'config_file_path': cls._config_file_path
+        }
+
+    def get_data(cls, value: str) -> dict:
+        if cls._check_if_url(cls.config_data.get(value)):
+            return cls.__download_url_data(cls.config_data.get(value))
+        else:
+            return cls.__read_from_disk(cls.config_data.get(value))
+
+    @property
+    def requests_kwargs(cls):
+        return cls._requests_kwargs
+
+    @requests_kwargs.setter
+    def requests_kwargs(cls, value):
+        cls._requests_kwargs = value
+
+    @property
+    def use_config(cls):
+        return cls._use_config
+
+    @use_config.setter
+    def use_config(cls, value):
+        cls._use_config = bool(value)
+
+    @property
+    def save_config(cls):
+        return cls._save_config
+
+    @save_config.setter
+    def save_config(cls, value):
+        cls._save_config = bool(value)
+
+    @property
+    def config_file_path(cls):
+        return cls.__get_absolute_path(cls._config_file_path)
+
+    @config_file_path.setter
+    def config_file_path(cls, value):
+        cls._config_file_path = cls.__get_absolute_path(value)
+        cls.__update_config()
+
+    @property
+    def data_path(cls):
+        return cls.__get_absolute_path(cls._data_path)
+
+    @data_path.setter
+    def data_path(cls, value):
+        cls._data_path = cls.__get_absolute_path(value)
+        cls.__update_config()
+
+    @property
+    def config_data(cls):
+        if cls.use_config:
+            cls.__config_data = cls.__read_from_disk(cls.config_file_path)
+            if not cls.__config_data:
+                cls.__update_config()
+        else:
+            cls.__update_config()
+        if cls.save_config:
+            cls.__update_config()
+            cls.__write_to_disk(cls.config_file_path, cls.__config_data)
+            cls._save_json_data()
+        return cls.__config_data
+
+    @property
+    def enterprise_attck_json(cls):
+        return cls._enterprise_attck_json
+
+    @enterprise_attck_json.setter
+    def enterprise_attck_json(cls, value):
+        cls._enterprise_attck_json = cls.__validate_value_string(value)
+        cls.__update_config()
+
+    @property
+    def pre_attck_json(cls):
+        return cls._pre_attck_json
+
+    @pre_attck_json.setter
+    def pre_attck_json(cls, value):
+        cls._pre_attck_json = cls.__validate_value_string(value)
+        cls.__update_config()
+
+    @property
+    def mobile_attck_json(cls):
+        return cls._mobile_attck_json
+
+    @mobile_attck_json.setter
+    def mobile_attck_json(cls, value):
+        cls._mobile_attck_json = cls.__validate_value_string(value)
+        cls.__update_config()
+
+    @property
+    def nist_controls_json(cls):
+        return cls._nist_controls_json
+
+    @nist_controls_json.setter
+    def nist_controls_json(cls, value):
+        cls._nist_controls_json = cls.__validate_value_string(value)
+        cls.__update_config()
+
+    @property
+    def generated_attck_json(cls):
+        return cls._generated_attck_json
+
+    @generated_attck_json.setter
+    def generated_attck_json(cls, value):
+        cls._generated_attck_json = cls.__validate_value_string(value)
+        cls.__update_config()
+
+    @property
+    def generated_nist_json(cls):
+        return cls._generated_nist_json
+
+    @generated_nist_json.setter
+    def generated_nist_json(cls, value):
+        cls._generated_nist_json = cls.__validate_value_string(value)
+        cls.__update_config()
+
+
+class Configuration(object, metaclass=ConfigurationProperties):
+    _use_config = False
+    _save_config = False
+    _config_file_path = '~/pyattck/config.yml'
+    _data_path = '~/pyattck/data'
+    _enterprise_attck_json = "https://raw.githubusercontent.com/mitre/cti/master/enterprise-attack/enterprise-attack.json"
+    _pre_attck_json="https://raw.githubusercontent.com/mitre/cti/master/pre-attack/pre-attack.json"
+    _mobile_attck_json="https://raw.githubusercontent.com/mitre/cti/master/mobile-attack/mobile-attack.json"
+    _nist_controls_json="https://raw.githubusercontent.com/center-for-threat-informed-defense/attack-control-framework-mappings/master/frameworks/nist800-53-r4/stix/nist800-53-r4-controls.json"
+    _generated_attck_json="https://github.com/swimlane/pyattck/blob/master/generated_attck_data.json?raw=True"
+    _generated_nist_json="https://github.com/swimlane/pyattck/blob/master/attck_to_nist_controls.json?raw=True"
+    _requests_kwargs = {}
