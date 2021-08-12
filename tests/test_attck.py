78badebffc0d95139311de7ec7d3b690ab3c8c0d
Josh <josh.rickard@swimlane.com>
2021-08-12 16:11:54 -0500

    Updated links to new location stored in s3

--- tests/test_attck.py
+++ tests/test_attck.py
@@ -10,8 +10,8 @@ default_config_data = {
     'pre_attck_json': "https://raw.githubusercontent.com/mitre/cti/master/pre-attack/pre-attack.json",
     'mobile_attck_json': "https://raw.githubusercontent.com/mitre/cti/master/mobile-attack/mobile-attack.json",
     'nist_controls_json': "https://raw.githubusercontent.com/center-for-threat-informed-defense/attack-control-framework-mappings/master/frameworks/ATT%26CK-v9.0/nist800-53-r5/stix/nist800-53-r5-controls.json",
-    'generated_attck_json': "https://github.com/swimlane/pyattck/blob/master/generated_attck_data.json?raw=True",
-    'generated_nist_json': "https://github.com/swimlane/pyattck/blob/master/attck_to_nist_controls.json?raw=True",
+    'generated_attck_json': "https://swimlane-pyattck.s3.us-west-2.amazonaws.com/generated_attck_data.json",
+    'generated_nist_json': "https://swimlane-pyattck.s3.us-west-2.amazonaws.com/attck_to_nist_controls.json",
     'config_file_path': os.path.abspath(os.path.expanduser(os.path.expandvars('~/pyattck/config.yml'))),
 }
 
